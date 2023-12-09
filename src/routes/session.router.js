ef4404e06c5947ec361358f69213a8523cd64cfb
Leandro Romero <romerodisind@gmail.com>
2023-12-09 20:48:40 -0300

    new refactor passport y router of login

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -2,26 +2,22 @@ const express = require("express");
 const passport = require("passport");
 const sessionRouter = express.Router();
 
-sessionRouter.post(
-  "/login",
-  (req, res, next) => {
-    passport.authenticate("login", { session: false }, (err, user, info) => {
-      if (err) {
+sessionRouter.post("/login", (req, res, next) => {
+  passport.authenticate("login", { session: false }, (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    if (!user) {
+      return res.status(401).json({ message: info.message || "Unauthorized" });
+    }
+    req.logIn(user, { session: false }, (error) => {
+      if (error) {
         return res.status(500).json({ message: "Internal Server Error" });
       }
-      if (!user) {
-        return res.status(401).json({ message: info.message || "Unauthorized" });
-      }
-      req.logIn(user, { session: false }, (error) => {
-        if (error) {
-          return res.status(500).json({ message: "Internal Server Error" });
-        }
-        res.json({ user: user.user, token: user.token });
-      });
-    })(req, res, next);
-  }
-);
-
+      res.json({ user: user.user, token: user.token });
+    });
+  })(req, res, next);
+});
 
 sessionRouter.post(
   "/register",
